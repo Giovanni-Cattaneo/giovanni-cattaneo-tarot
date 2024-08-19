22ae5a853d4b9b1468ceac27853dbae4bf9ee7b2
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-08-19 16:33:50 +0200

    add state and move cards array

---
+++ src/components/appCard.vue
@@ -0,0 +1,16 @@
+<script>
+import { state } from './state.js'
+
+export default {
+    name: "appCard",
+    data() {
+        return {
+            state
+        }
+    }
+}
+</script>
+
+<template></template>
+
+<style></style>
